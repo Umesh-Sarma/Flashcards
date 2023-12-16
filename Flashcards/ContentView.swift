5d5262bdeb60e2ba1e706f75fa5548a8c3fa99ff
Umesh Sarma <usarma1200@gmail.com>
2023-12-16 14:06:09 -0600

    this is a new project. I forgot the dev branch initially. Added a box where the user can type a question. Also created another screen.

--- Flashcards/ContentView.swift
+++ Flashcards/ContentView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var userQuestion: String = ""
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+
+                TextField("Type your question here", text: $userQuestion)
+                    .padding()
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                NavigationLink(destination: FlashcardView(question: userQuestion)) {
+                    Text("Create Flashcard")
+                        .padding()
+                        .background(Color.accentColor)
+                        .foregroundColor(.white)
+                        .cornerRadius(8)
+                }
+            }
+            .padding()
+            .navigationTitle("FlashCards App")
         }
-        .padding()
     }
 }
+
+struct FlashcardView: View {
+    var question: String
+
+    var body: some View {
+        Text("Your Question: \(question)")
+            .padding()
+            .navigationTitle("Flashcard")
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
