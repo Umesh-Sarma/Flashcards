09d14df1d935fd307fbf0cd08a03277258c4795c
Umesh Sarma <usarma1200@gmail.com>
2023-12-16 13:59:14 -0600

    Initial Commit

---
+++ Flashcards/FlashcardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashcardsApp.swift
+//  Flashcards
+//
+//  Created by Umesh Sarma on 12/16/23.
+//
+
+import SwiftUI
+
+@main
+struct FlashcardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
